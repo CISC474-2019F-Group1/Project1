4541bd05b0ce009a561c1bff41d4d67b2e6f2e17
ZHIHAO <zzhsaga@gmail.com>
2019-09-18 19:46:46 -0400

    Revert "ball and computer player"

--- scripts/ball.js
+++ scripts/ball.js
@@ -5,43 +5,4 @@ function ball() {
     var startX;
     var startY;
 
-    this.positionX = 100;
-    this.positionY = 300;
-    this.speedX = 3;
-    this.speedY = 3;
-    this.size = 25;
-    this.maxwidth = window.innerWidth - this.size;
-    this.maxheight = window.innerHeight - this.size;
-    
-    $('#ball').css("width", this.width);
-
-    this.updatePosition = function(paddle){
-        if(this.positionY <= 30){
-            //new turn trigger will be here
-            this.positionX=100;
-            this.positionY=300;
-        }
-        else if(this.positionX > this.maxwidth||this.positionX < 0){
-            this.speedX = -this.speedX;
-        }
-        else if(this.positionY> this.maxheight){
-            this.speedY = -this.speedY;
-        }
-        else if (this.positionX>paddle-50&&this.positionX<paddle+200){
-            //console.log('pass check');
-            if(this.positionY<40+this.size){
-                console.log('collision at: ('+this.positionX+','+this.positionY+')' );
-                this.speedY = -this.speedY;
-            }
-        }
-        this.positionX += this.speedX;
-        this.positionY += this.speedY;
-
-
-        
-       
-        
-    }
-    
-
 }
