7b33a40934a684b6ab2d6e908ac85aeee7797685
302Blue <abaldwin@udel.edu>
2019-09-29 20:22:07 -0400

    Small formatting changes

--- scripts/src/powerup.ts
+++ scripts/src/powerup.ts
@@ -1,4 +1,4 @@
-class Powerup {
+class PowerUp {
 
     /*
        0 - no powerup
@@ -15,88 +15,68 @@ class Powerup {
     radius: number;
 
     constructor(p: number, d: number) {
-        
         this.pid = p;
         this.duration = d;
         this.state = 'block';
         this.x = 0;
         this.y = 0;
-        this.radius = 15
-
+        this.radius = 15;
     }
 
-    getX(){
-        return this.x;
-    }
-
-    getY(){
-        return this.y;
-    }
-
-    getPid(){
-        return this.pid;
-    }
-
-    getState(){
-        return this.state;
-    }
-
-    place(startX: number, startY: number){
-
-        if((this.state === 'block') && (this.pid != 0)){
+    place(startX: number, startY: number) {
+        if ((this.state === 'block') && (this.pid != 0)) {
             this.x = startX;
             this.y = startY;
             this.state = 'falling';
         }
-
     }
 
-    activate(){
-
-        if(this.state === 'falling'){
-
+    activate() {
+        if (this.state === 'falling') {
             this.x = -100;
             this.y = -100;
             this.state = 'activated';
-
         }
+    }
 
-    } 
-
-    updatePowerup(){
-
-        if(this.state === 'falling'){
-
-            this.y -= 1;
-
-        }
-
-        if(this.checkCollideRect(paddle)){
-
-            console.log("Collided");
-
-        }
-
+    updatePowerUp() {
+        if (this.state === 'falling') { this.y -= 1 }
+        if (this.checkCollideRect(paddle)) { console.log("Collided") }
     }
 
     checkCollideRect(rect: Rectangle) {
         let testX: number = this.x;
         var testY: number = this.y;
-        
-        if      (this.x < rect.getLeftX())   { testX = rect.getLeftX() }
-        else if (this.x > rect.getRightX())  { testX = rect.getRightX() }
-        if      (this.y < rect.getBottomY()) { testY = rect.getBottomY() }
-        else if (this.y > rect.getTopY())    { testY = rect.getTopY() }
-        
+
+        if (this.x < rect.getLeftX()) {
+            testX = rect.getLeftX();
+        } else if (this.x > rect.getRightX()) {
+            testX = rect.getRightX();
+        }
+        if (this.y < rect.getBottomY()) {
+            testY = rect.getBottomY();
+        }
+        else if (this.y > rect.getTopY()) {
+            testY = rect.getTopY();
+        }
+
         let distX: number = this.x - testX;
         let distY: number = this.y - testY;
         let distance: number = Math.sqrt(distX * distX + distY * distY);
-        
+
         return {
-            "hit" : distance <= this.radius,
-            "distX" : distX,
-            "distY" : distY
+            "hit": distance <= this.radius,
+            "distX": distX,
+            "distY": distY
         };
     }
+
+    getX() { return this.x }
+
+    getY() { return this.y }
+
+    getPid() { return this.pid }
+
+    getState() { return this.state }
 
 }
