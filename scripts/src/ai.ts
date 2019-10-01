533e6184418bc2c4708761e6ae49e33a7127e784
Zhihao Zhang <zzhsaga@gmail.com>
2019-10-01 11:54:08 -0400

    commit

--- scripts/src/ai.ts
+++ scripts/src/ai.ts
@@ -1,4 +1,5 @@
-
+// tf....
+// Three layers model
 // const model = tf.sequential([
 //     Dense({units: 128, inputShape: [8]}),
 //     Dense({units: 256, inputShape: [128], activation:"sigmoid"}),
@@ -8,22 +9,20 @@
 // const optimizer = tf.train.adam(learningRate);
 // model.compile({loss: 'meanSquaredError', optimizer: optimizer});
 class AI {
-    previous_data: number[];
+    pre_data: number[];
     training_data: [any[], any[], any[], any[]];
     last_data: any[];
     turn: number;
     grab_data: boolean;
-    flip_table: boolean;
     data_xs: number[];
 
     constructor() {
-        this.previous_data = [0, 0, 0, 0];
+        this.pre_data = [0, 0, 0, 0];
         this.training_data = [[], [], [], []];
         this.turn = 0;
         this.grab_data = true;
-        this.flip_table = true;
         this.data_xs = [];
-        this.last_data = [...this.previous_data, ...this.data_xs];
+        this.last_data = [...this.pre_data, ...this.data_xs];
     }
 
     save_data(paddle: Paddle, ball: Ball, gameState: GameState) {
@@ -31,19 +30,19 @@ class AI {
             return;
         }
 
-        if (this.previous_data == null) {
+        if (this.pre_data == null) {
             const data = [paddle.getLeftX(), ball.getX(), ball.getY(), gameState.getScore()];
-            this.previous_data = data;
+            this.pre_data = data;
             return;
         }
 
         const data_xs = [paddle.getLeftX(), ball.getX(), ball.getY(), gameState.getScore()];
-        const index = (paddle.getLeftX() < this.previous_data[0]) ? 0 : ((paddle.getLeftX() == this.previous_data[0]) ? 1 : 2);
+        const index = (paddle.getLeftX() < this.pre_data[0]) ? 0 : ((paddle.getLeftX() == this.pre_data[0]) ? 1 : 2);
 
 
-        this.last_data = [...this.previous_data, ...data_xs];
+        this.last_data = [...this.pre_data, ...data_xs];
         this.training_data[index].push(this.last_data);
-        this.previous_data = data_xs;
+        this.pre_data = data_xs;
         // console.log('last_data',this.last_data);
         // console.log('training_data',this.training_data);
         // console.log('previous_data',this.previous_data);
@@ -79,14 +78,18 @@ class AI {
 
     predict(paddle: Paddle, ball: Ball, board: Board) {
         //approximately logistic regression
-        let y = 0.33 * ball.getX() - 0.297 * paddle.getX() + board.getRightEdgeX() * 0.00671;
-        let w = 1 / (1 + Math.E ** (-y))
-        if (w > 1 / 2) {
-            console.log('prediction', y, w);
+        //even perform better...
+        let wx = 0.33 * ball.getX() - 0.297 * paddle.getX() + board.getRightEdgeX() * 0.00671;
+        //map to 0,1 by sigmoid function
+        //provide more smoothy reaction
+        let y = 1 / (1 + Math.E ** (-wx))
+
+        if (y > 1 / 2) {
+            // console.log('prediction', y, w);
             return 'right';
         }
-        else if (w < 1 / 2) {
-            console.log('prediction', y, w);
+        else if (y < 1 / 2) {
+            // console.log('prediction', y, w);
             return 'left';
         }
         return 'hold';
